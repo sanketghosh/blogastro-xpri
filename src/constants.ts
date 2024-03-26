3882d8f69bfa4b7464bb715aacbfd39f5f832976
sanket <code.sanket13@gmail.com>
2024-03-27 00:35:30 +0530

    refactor: design changes

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const SITE_META_DATA = {
+  title: "TechBlog",
+  description:
+    "Articles, stories, tutorials and more for tech people by tech people.",
+};
+
+export const HOME_PAGE_ARTICLE_LIMIT = 3;
+export const ARTICLES_PER_PAGE = 6;
